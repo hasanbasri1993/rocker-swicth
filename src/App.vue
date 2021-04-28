3e0d6ce27837f98e3434d990296c1b523480ed3c
Hasan Basri <hasanbasri1493@gmail.com>
2021-04-28 13:10:06 +0700

    set

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
-    <Lightbulb :isOn="currentValue"/>
+    <Lightbulb :isOn="currentValue" />
     <div>
       <p>Simple usage</p>
       <div>
-        <RockerSwitch :size="0.9" :value="currentValue" @change="isOn => (currentValue = isOn)"/>
+        <RockerSwitch
+          :size="0.9"
+          :value="currentValue"
+          @change="(isOn) => (currentValue = isOn)"
+        />
       </div>
       <code>{{ currentValue }}</code>
     </div>
@@ -12,6 +16,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import Lightbulb from "./Lightbulb";
 import RockerSwitch from "vue-rocker-switch";
 import "vue-rocker-switch/dist/vue-rocker-switch.css";
@@ -20,13 +25,24 @@ export default {
   name: "App",
   components: {
     RockerSwitch,
-    Lightbulb
+    Lightbulb,
   },
   data() {
     return {
-      currentValue: true
+      currentValue: true,
     };
-  }
+  },
+  methods: {
+    get_status: function () {
+      axios
+      .get("https://blynk.daarululuumlido.com:9443/M_jB0GbW09rwrHfZN4WaLv8BloEGIZZ9/get/V2")
+      .then((response) => (console.log(response)));
+      setInterval(() => this.get_status(), 3);
+    }
+  },
+  mounted() {
+    this.get_status()
+  },
 };
 </script>
 
